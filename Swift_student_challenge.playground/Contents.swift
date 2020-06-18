ae104d64d88f7eec5702298f4f4ba391ae7875da
Antonio Carlos <antonio.correa0407@gmail.com>
2020-06-18 19:44:58 -0300

    Preparing the project to WWDC 2020

---
+++ Swift_student_challenge.playground/Contents.swift
@@ -0,0 +1,60 @@
+/*:
+ 
+ # What is chemystry?
+ 
+ Atoms, molecules, complex formulas, this is the foundation of chemistry, but for those who think that this is only a matter of research in the laboratory, they were wrong!
+ 
+ Chemistry is present in our daily lives more than we think. Our body is made up entirely of atoms and molecules. Everything that surrounds us is also composed of several elements, but in different states and compositions. In short, everything around you and inside you works and exists because of chemistry!
+ 
+ In chemistry there are several important concepts, but two of them are fundamental to understand how it works. First, atoms, which are the smallest particles of anything that exists and when I say smaller, they are even smaller. For you to understand how small they are, a single grain of sand is made up of billions of atoms. Second, it is the molecules, which are sets of atoms, and even that set is extremely small.
+ 
+ These atoms and molecules mix, react and cause chemical reactions, a process that happens all the time in our universe and everyday. And in this project you will be able to see a chemical process that occurs every day inside our mouth !!
+ 
+ # Chemical Process in Human Mouth
+ 
+ Do you know what happens when we eat? Have you ever wondered why we have to keep oral hygiene up to date? Come with me and I'll explain!
+
+ Our mouth is a very favorable environment for the proliferation of bacteria, due to its temperature around 36˚C, it is always moist and has a high flow of nutrients due to our food. When we eat or drink, these foods have sugars that are deposited in our mouth and these sugars serve as food for the bacteria that increase their proliferation. In short, the more sugars the food has, the more the bacteria will reproduce.
+
+ Based on this explanation, you know that the longer we go without brushing our teeth after having a meal, the more bacteria we will have in our mouth and this can generate the famous caries. But what is the chemical process that occurs until it generates caries?
+
+ The chemical process that generates caries is called demineralization where the main component of tooth enamel is dissolved by the level of acidity in the mouth decreasing as the proliferation of bacteria increases.
+
+ However, this process can be reversed as long as we maintain constant oral hygiene, even when we spend a long time without eating. Now you will be able to see in practice how this chemical process occurs.
+ 
+ # Credits (Sound Effects)
+ 
+ - - -
+ 
+ **Author**: morgantj
+ 
+ **Sound Name**: slurping_then_an_ah
+ 
+ [Link to the sound](https://freesound.org/people/morgantj/sounds/58847/)
+ 
+ **Explanation**: I used this sound effect to make a coolest interaction when putting the soda into the mouth
+ 
+ - - -
+ 
+ **Author**: vedm
+ 
+ **Sound Name**: sandwich sounds volume 1 fixed
+ 
+ [Link to the sound](https://freesound.org/people/vedm/sounds/370214/)
+ 
+ **Explanation**: I used this sound effect to make a coolest interaction when putting the sandwich into the mouth
+ 
+ - - -
+ 
+ */
+
+import PlaygroundSupport
+import SpriteKit
+
+let skView = SKView(frame: NSRect(x: 0, y: 0, width: 700, height: 500))
+
+let gameScene = GameScene(size: skView.frame.size)
+
+skView.presentScene(gameScene)
+
+PlaygroundSupport.PlaygroundPage.current.liveView = skView
